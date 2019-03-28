b04968adb00fe64ac12a57541c89b10a8d10840b
Gunock <asz.czyk@gmail.com>
2019-03-28 19:32:17 +0100

    Add UART receive with interrupt

--- Inc/UART.hpp
+++ Inc/UART.hpp
@@ -12,28 +12,40 @@
 #include <string>
 
 class UART final {
-	UART_HandleTypeDef* handle;
+    UART_HandleTypeDef *handle;
 
 public:
-	explicit UART() noexcept : handle(nullptr) {}
-	explicit UART(UART_HandleTypeDef* huartx) noexcept;
-	~UART() = default;
+    explicit UART() noexcept : handle(nullptr) {}
 
-	void init(UART_HandleTypeDef* huartx);
-	UART_HandleTypeDef* getHandle();
-	const UART_HandleTypeDef* getHandle() const;
+    explicit UART(UART_HandleTypeDef *huartx) noexcept;
 
-	bool transmit(uint8_t *tData, const uint16_t &size) const;
-	bool transmit(const char *tData, const uint16_t &size) const;
-	bool transmit(const char* tData) const;
-	bool transmit(const std::string tData) const;
+    ~UART() = default;
 
-	void discardAll() const;
+    void init(UART_HandleTypeDef *huartx);
 
-	bool receive(uint8_t *rData, const uint16_t &size) const;
-	bool receive(char *rData, const uint16_t &size) const;
-	bool receive(std::string& rData, const uint16_t &size) const;
-	void receiveAll(std::string& rData);
+    UART_HandleTypeDef *getHandle();
+
+    const UART_HandleTypeDef *getHandle() const;
+
+    bool transmit(uint8_t *tData, const uint16_t &size) const;
+
+    bool transmit(const char *tData, const uint16_t &size) const;
+
+    bool transmit(const char *tData) const;
+
+    bool transmit(const std::string &tData) const;
+
+    void discardAll() const;
+
+    bool receive(uint8_t *rData, const uint16_t &size) const;
+
+    bool receive(char *rData, const uint16_t &size) const;
+
+    bool receive(std::string &rData, const uint16_t &size) const;
+
+    void receiveAll(std::string &rData);
+
+    void receiveIt(char* rData, const uint16_t &size) const;
 };
 
 #endif /* UART_HPP_ */
